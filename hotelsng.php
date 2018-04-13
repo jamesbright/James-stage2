3e2cfa75398573582821b4f2006cd58f3603384c
jamesbright <jamesononiwu@gmail.com>
2018-04-14 00:50:52 +0100

    Add files via upload
    Hotelsng-stage1 simple page design

---
+++ hotelsng.php
@@ -0,0 +1,93 @@
+<?php
+
+
+
+
+
+
+?>
+<!DOCTYPE html>
+
+<html>
+<head>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+
+<link href="bootstrap.min.css" rel="stylesheet" />
+
+
+
+<link href="styles.css" rel="stylesheet" />
+
+
+
+</head>
+
+<body>
+
+
+
+<style>
+body{
+	
+	background-image:url("img3.jpg") !important;
+	background-repeat:no-repeat;
+	background-size:100% 800px;
+}
+.date-time{
+	margin-top:20px;
+	margin-left:40px;
+	margin-right:40px;
+	text-align:center;
+	height:150px;
+	width:300px;
+	color:navajowhite !important;
+	font-weight:bold;
+	font-size:20px;
+	
+	background-image:url("img1.jpg");
+	background-repeat:no-repeat;
+}
+.navbar-fixed-top{
+	text-align:center;
+	background-color:navajowhite;
+	padding-left:50%;
+}
+
+</style>
+
+<div class="container-fluid">
+<div class="nav navbar navbar-fixed-top navbar-inverse">
+<h3 >HOTELS.NG</div>
+
+</div>
+
+<br><br>
+<div class="date-time">
+
+<?php
+echo "HOTELS.NG INTERNSHIP 4.<br> 
+<font style='color:white' size=-2 >Transforming Ideas Into Reality </font><br><br>
+<span 
+style='color:white'>".date('h:i:s a')."</span>";
+
+?>
+</div>
+
+
+
+</div>
+
+
+
+<script src="bootstrap.min.js" ></script>
+
+
+
+<script src="jquery.js" ></script>
+</body>
+
+</html>
